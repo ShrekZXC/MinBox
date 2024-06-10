db10b17784996ac25692cc637ac7c9afacef29d3
dima.bogatyrev.2001@gmail.com <123zxc456vbnSQ>
2024-06-10 09:50:31 +0700

    add CircleTest

---
+++ ShapeTests/CircleTests.cs
@@ -0,0 +1,22 @@
+﻿using ShapeLibrary;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace ShapeTests;
+
+public class CircleTests
+{
+    [Fact]
+    public void GetArea_ValidRadius_ReturnsCorrectArea()
+    {
+        var circle = new Circle(5);
+        double area = circle.GetArea();
+        Assert.Equal(Math.PI * 25, area, precision:5);
+    }
+
+    [Fact]
+    public void Constructor_NegativeRadius_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => new Circle(-5));
+    }
+}
